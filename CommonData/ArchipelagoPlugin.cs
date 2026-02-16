21c2f78999146e2bfba0a3b15cbe241ff2895d09
thedrummonger <thedrummonger@DESKTOP>
2026-02-16 14:51:18 -0600

    Experimental AP menu key rebinding.

--- CommonData/ArchipelagoPlugin.cs
+++ CommonData/ArchipelagoPlugin.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 
 namespace YargArchipelagoPlugin
@@ -16,6 +18,11 @@ namespace YargArchipelagoPlugin
         public const string pluginName = "YARG Archipelago Plugin";
 #endif
         public static APConnectionContainer APcontainer;
+
+        private ConfigEntry<Key> toggleKey;
+        private ConfigEntry<bool> requireCtrl;
+        private ConfigEntry<bool> requireShift;
+        private ConfigEntry<bool> requireAlt;
         public void Awake()
         {
             var patcher = new Harmony(pluginGuid);
@@ -24,14 +31,28 @@ namespace YargArchipelagoPlugin
             Logger.LogInfo("Starting AP");
             APcontainer = new APConnectionContainer(Logger);
 
+            toggleKey = Config.Bind("Hotkeys", "ToggleDialogKey", Key.F10, "Keyboard key used to toggle the connection dialog.");
+
+            requireCtrl = Config.Bind("Hotkeys", "ToggleDialogRequireCtrl", false, "Require Ctrl to be held.");
+            requireShift = Config.Bind("Hotkeys", "ToggleDialogRequireShift", false, "Require Shift to be held.");
+            requireAlt = Config.Bind("Hotkeys", "ToggleDialogRequireAlt", false, "Require Alt to be held.");
+
         }
 
         private void Update()
         {
-            if (!Application.isFocused) 
+            if (!Application.isFocused)
                 return;
 
-            if (UnityEngine.InputSystem.Keyboard.current != null && UnityEngine.InputSystem.Keyboard.current.f10Key.wasPressedThisFrame)
+            var kb = Keyboard.current;
+            if (kb == null)
+                return;
+
+            if (requireCtrl.Value && !(kb.leftCtrlKey.isPressed || kb.rightCtrlKey.isPressed)) return;
+            if (requireShift.Value && !(kb.leftShiftKey.isPressed || kb.rightShiftKey.isPressed)) return;
+            if (requireAlt.Value && !(kb.leftAltKey.isPressed || kb.rightAltKey.isPressed)) return;
+
+            if (kb[toggleKey.Value].wasPressedThisFrame)
                 ToggleArchipelagoDialog();
         }
         public static void ToggleArchipelagoDialog()
